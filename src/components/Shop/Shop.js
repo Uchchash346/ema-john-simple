e7861c620b9c2960413f8b381466e59b709cc875
Uchchash346 <mru416@gmail.com>
2021-02-26 19:51:00 +0600

    first commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
@@ -8,7 +9,6 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     const handleAddProduct = (product) =>{
-        console.log('Product added', product);
         const newCart = [...cart, product];
         setCart(newCart);
     }
@@ -23,8 +23,7 @@ const Shop = () => {
                 }
             </div>
            <div className="cart-container">
-               <h3>This is cart</h3>
-               <h5>Order Summary: {cart.length}</h5>
+              <Cart cart={cart}></Cart>
            </div>
             
         </div>
